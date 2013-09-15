3fc411b3e857046a2159902b583a51acfd9384e9
chenjiebin <chenjiebin@iceup>
2013-09-16 00:07:48 +0800

    git数据模型

--- php/yaf/application/controllers/Index.php
+++ php/yaf/application/controllers/Index.php
@@ -1,6 +1,33 @@
 <?php
-class IndexController extends Yaf_Controller_Abstract {
-   public function indexAction() {//默认Action
-       $this->getView()->assign("content", "Hello World");
-   }
+
+class IndexController extends \Yaf\Controller_Abstract {
+
+    public function indexAction() {//默认Action
+        $this->getView()->assign("content", "Hello World");
+    }
+    
+    public $config = null;
+    
+    public function testdbAction() {
+        
+         $config = \Yaf\Application::app()->getConfig();
+
+        $this->config = $config;
+        
+        $conf = $this->config->get('resources.database.params');
+
+        if (!$conf)
+            return false;
+
+        $dbAdapter = new \Zend\Db\Adapter\Adapter($conf->toArray());
+        
+        \Yaf\Registry::set('dbAdapter', $dbAdapter);
+        
+        $userMapper  = new \Mapper\UserModel();
+        $model = $userMapper->find(1);
+        
+        
+        $this->getView()->assign("content", "test db");
+    }
+
 }
---
+++ php/yaf/application/models/Mapper/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mapper;
+
+/**
+ * model mapper.
+ *
+ * 行为分析
+ *
+ * @package Mapper
+ */
+class UserModel extends \Mapper\AbstractModel {
+
+    protected $_tableName = 'user';
+//    protected $_dbModelClass = '\BehaviorModel';
+
+}
