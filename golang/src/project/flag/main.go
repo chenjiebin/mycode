2a8abdb69f8e443f68a7b5cc92945f485a30814b
chenjiebin <sjlinyu@qq.com>
2014-08-18 23:02:16 +0800

    golang command args

--- golang/src/project/flag/main.go
+++ golang/src/project/flag/main.go
@@ -5,11 +5,16 @@ import (
 	"fmt"
 )
 
-var (
-	port = flag.String("port", ":8080", "http listen port")
-)
-
 func main() {
+	var pwd string
+
+	port := flag.String("port", ":8080", "http listen port")
+	user := flag.String("user", "root", "mysql connect user")
+	flag.StringVar(&pwd, "password", "123", "mysql connect password")
+
 	flag.Parse()
+
 	fmt.Println(*port)
+	fmt.Println(*user)
+	fmt.Println(pwd)
 }
