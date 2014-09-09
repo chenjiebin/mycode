d5a8041256c73806e61d7b72d724e0ca0b7be05b
chenjiebin <sjlinyu@qq.com>
2014-09-09 17:24:53 +0800

    时间戳

--- golang/src/project/time/main.go
+++ golang/src/project/time/main.go
@@ -6,9 +6,20 @@ import (
 )
 
 func main() {
+	formatTime()
+	fmt.Println(getTodayStartTimeUnix())
+}
+
+func formatTime() {
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
 
 	t := time.Now().Unix()
-
+	fmt.Println(t)
 	fmt.Println(time.Unix(t, 0).String())
+}
+
+//获取今天开始时间戳
+func getTodayStartTimeUnix() int64 {
+	t, _ := time.Parse("2006-01-02 15:04:05", time.Now().Format("2006-01-02")+" 00:00:00")
+	return t.Unix() - 8*3600
 }
