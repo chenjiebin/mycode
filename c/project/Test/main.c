b7450f4069387113c28a3fae4359016fa5fdde88
chenjiebin <sjlinyu@qq.com>
2014-10-16 12:49:17 +0800

    c语言

---
+++ c/project/Test/main.c
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.c
+ * Author: chenjiebin
+ *
+ * Created on October 16, 2014, 12:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    printf("Hello, world.\n");
+
+    return (EXIT_SUCCESS);
+}
+
