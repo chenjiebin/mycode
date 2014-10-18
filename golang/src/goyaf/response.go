29324168223274ec9d7f8f5e642ee209ab1a09e6
chenjiebin <sjlinyu@qq.com>
2014-10-19 00:47:08 +0800

    resonse change

--- golang/src/goyaf/response.go
+++ golang/src/goyaf/response.go
@@ -8,28 +8,22 @@ import (
 //返回对象
 type Response struct {
 	w     http.ResponseWriter
-	body  interface{}
 	bodys []interface{}
 }
 
-func (this *Response) SetBody(body interface{}) {
-	this.AppendBody(body)
-}
-
-func (this *Response) GetBody() interface{} {
-	return this.body
-}
-
+//追加body
 func (this *Response) AppendBody(body interface{}) {
 	this.bodys = append(this.bodys, body)
 }
 
+//返回数据
 func (this *Response) Response() {
 	for _, body := range this.bodys {
 		fmt.Fprintln(this.w, body)
 	}
 }
 
+//设置cookie
 func (this *Response) SetCookie(cookie *http.Cookie) {
 	http.SetCookie(this.w, cookie)
 }
