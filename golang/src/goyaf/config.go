9ffe8bed47d6a2be5734dc8b3436e3cd2e0925ba
chenjiebin <sjlinyu@qq.com>
2014-10-19 13:52:07 +0800

    配置获取调整

--- golang/src/goyaf/config.go
+++ golang/src/goyaf/config.go
@@ -1,13 +1,13 @@
 package goyaf
 
-var Config map[string]map[string]string
+var Config map[string]string
 
-func SetConfig(config map[string]map[string]string) {
+func SetConfig(config map[string]string) {
 	Config = config
 
 	//Debug(Config)
 }
 
 func GetConfigByKey(key string) string {
-	return Config["devel"][key]
+	return Config[key]
 }
