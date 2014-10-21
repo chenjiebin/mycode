e7ef7004182da03f9858ec94f2f3cf54aa551367
chenjiebin <sjlinyu@qq.com>
2014-10-21 19:00:24 +0800

    测试

--- golang/src/project/tmp.go
+++ golang/src/project/tmp.go
@@ -5,13 +5,23 @@ import (
 )
 
 func main() {
-	params := map[string]string{
-		"page": "",
-		"name": "cjb",
-	}
-	checkPageAndCount(&params)
+	name := "哈ab哈哈哈"
+	ru := []rune(name)
 
-	fmt.Println(params)
+	fmt.Println(len(ru))
+
+	for _, r := range ru {
+		fmt.Println(string(r))
+	}
+	//fmt.Println(len("爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸"))
+	//params := map[string]string{
+	//	"page": "",
+	//	"name": "cjb",
+	//}
+	//checkPageAndCount(&params)
+	//爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸
+	//fmt.Println(params)
+	//爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸
 }
 
 //检测page和count参数，如果没有值赋予默认值1和10
