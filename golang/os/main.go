1d080e34d534060c8d864984d981b832b77af653
chenjiebin <sjlinyu@qq.com>
2014-10-27 20:59:47 +0800

    测试

--- golang/os/main.go
+++ golang/os/main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"reflect"
 )
 
-func main() {
-	pid := os.Getppid()
+type base struct {
+	age int
+}
 
-	fmt.Println(pid)
+//type controller interface {
+//	Hello()
+//}
+
+func (this *base) Hello() {
+	fmt.Println("hello")
+	this.age = 10
+
+}
+
+type user struct {
+	base
+	Id   int64
+	name string
+}
+
+func (this user) To() {
+	this.name = "hehe"
+}
+
+func main() {
+	var u interface{}
+	u = user{Id: 1, name: "cjb"}
+	fmt.Println(reflect.ValueOf(&u).Elem())
+	fmt.Println(u)
+
+	//u2 := u
+	//u3 := &u2
+
+	//fmt.Println(c)
+	//fmt.Println(u2)
+
 }
--- golang/tmp.go
+++ golang/tmp.go
@@ -2,31 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
-func main() {
-	name := "哈ab哈哈哈"
-	ru := []rune(name)
+/*获取当前文件执行的路径*/
+func GetCurrPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+	fmt.Println(file)
 
-	fmt.Println(len(ru))
-
-	for _, r := range ru {
-		fmt.Println(string(r))
-	}
-	//fmt.Println(len("爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸"))
-	//params := map[string]string{
-	//	"page": "",
-	//	"name": "cjb",
-	//}
-	//checkPageAndCount(&params)
-	//爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸
-	//fmt.Println(params)
-	//爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸
+	path, _ := filepath.Abs(file)
+	fmt.Println(path)
+	splitstring := strings.Split(path, "\\")
+	size := len(splitstring)
+	splitstring = strings.Split(path, splitstring[size-1])
+	ret := strings.Replace(splitstring[0], "\\", "/", size-1)
+	return ret
 }
 
-//检测page和count参数，如果没有值赋予默认值1和10
-//因为page和count参数都是在列表的时候才需要传递，所以这里直接计算出offset
-func checkPageAndCount(params *map[string]string) {
-	(*params)["page"] = "1"
+func main() {
+	fmt.Println(GetCurrPath())
 
 }
