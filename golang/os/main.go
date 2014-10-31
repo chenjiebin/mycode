1ea28c80c4ba082b2f0e7c7ae51dadadf3ed3872
chenjiebin <sjlinyu@qq.com>
2014-10-31 22:29:07 +0800

    测试代码

--- golang/os/main.go
+++ golang/os/main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-func main() {
-	pid := os.Getppid()
+type User struct {
+	Id   int
+	Name string
+}
 
-	fmt.Println(pid)
+func (this *User) SetName() {
+	this.Name = "hehe"
+}
+
+func main() {
+	users := map[string]User{
+		"1": User{Id: 1, Name: "cjb"},
+	}
+
+	user := users["1"]
+
+	user2 := &user
+
+	fmt.Println(user2)
+	user2.Id = 2
+	fmt.Println(user2)
+	user2.SetName()
+	fmt.Println(user2)
+
+	fmt.Println(users)
 }
--- golang/tmp.go
+++ golang/tmp.go
@@ -1,32 +1,36 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func main() {
-	name := "哈ab哈哈哈"
-	ru := []rune(name)
-
-	fmt.Println(len(ru))
-
-	for _, r := range ru {
-		fmt.Println(string(r))
+func a() (err error) {
+	err = b()
+	if err != nil {
+		return
 	}
-	//fmt.Println(len("爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸"))
-	//params := map[string]string{
-	//	"page": "",
-	//	"name": "cjb",
-	//}
-	//checkPageAndCount(&params)
-	//爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸
-	//fmt.Println(params)
-	//爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸爸
+	err = c()
+	if err != nil {
+		return
+	}
+	err = errors.New("a内错误")
+	return
 }
 
-//检测page和count参数，如果没有值赋予默认值1和10
-//因为page和count参数都是在列表的时候才需要传递，所以这里直接计算出offset
-func checkPageAndCount(params *map[string]string) {
-	(*params)["page"] = "1"
+func b() (err error) {
+	err = errors.New("b内错误")
+	return
+}
 
+func c() (err error) {
+	err = errors.New("c内错误")
+	return
+}
+
+func main() {
+	err := a()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
