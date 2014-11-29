a34515746818d8ae441f05748561f9ae4e0f1471
chenjiebin <sjlinyu@qq.com>
2014-11-29 15:41:03 +0800

    golang signal

--- golang/signal/main.go
+++ golang/signal/main.go
@@ -1,68 +1,20 @@
 package main
 
-import "fmt"
-import "time"
-import "os"
-import "os/signal"
-import "syscall"
-
-type signalHandler func(s os.Signal, arg interface{})
-
-type signalSet struct {
-	m map[os.Signal]signalHandler
-}
-
-func signalSetNew() *signalSet {
-	ss := new(signalSet)
-	ss.m = make(map[os.Signal]signalHandler)
-	return ss
-}
-
-func (set *signalSet) register(s os.Signal, handler signalHandler) {
-	if _, found := set.m[s]; !found {
-		set.m[s] = handler
-	}
-}
-
-func (set *signalSet) handle(sig os.Signal, arg interface{}) (err error) {
-	if _, found := set.m[sig]; found {
-		set.m[sig](sig, arg)
-		return nil
-	} else {
-		return fmt.Errorf("No handler available for signal %v", sig)
-	}
-
-	panic("won't reach here")
-}
+import (
+	"fmt"
+	"os"
+	"os/signal"
+)
 
 func main() {
-	go sysSignalHandleDemo()
-	time.Sleep(time.Hour) // make the main goroutine wait!
-}
+	//设置一个channel来发送信号
+	c := make(chan os.Signal, 1)
+	signal.Notify(c)
 
-func sysSignalHandleDemo() {
-	ss := signalSetNew()
-	handler := func(s os.Signal, arg interface{}) {
-		fmt.Printf("handle signal: %v\n", s)
-	}
+	// 一直运行一直到收到一个信号
 
-	ss.register(syscall.SIGINT, handler)
-	ss.register(syscall.SIGUSR1, handler)
-	ss.register(syscall.SIGUSR2, handler)
+	s := <-c
 
-	for {
-		c := make(chan os.Signal)
-		var sigs []os.Signal
-		for sig := range ss.m {
-			sigs = append(sigs, sig)
-		}
-		signal.Notify(c)
-		sig := <-c
+	fmt.Println("Got signal:", s) //当我停止运行时 Got signal: interrupt
 
-		err := ss.handle(sig, nil)
-		if err != nil {
-			fmt.Printf("unknown signal received: %v\n", sig)
-			os.Exit(1)
-		}
-	}
 }
