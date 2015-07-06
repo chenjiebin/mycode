0c50a9e6cf323b4f105827c4f8c58db86cea9bb2
chenjiebin <sjlinyu@qq.com>
2015-07-06 21:58:27 +0800

    golang test

--- golang/test/src/lib/fibonacci_test.go
+++ golang/test/src/lib/fibonacci_test.go
@@ -18,3 +18,17 @@ func BenchmarkFibonacci(b *testing.B) {
 		Fibonacci(10)
 	}
 }
+
+//测试参数为5的性能
+func BenchmarkFibonacci5(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Fibonacci(5)
+	}
+}
+
+//测试参数为20的性能
+func BenchmarkFibonacci20(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Fibonacci(20)
+	}
+}
