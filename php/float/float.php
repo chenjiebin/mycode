21495672eda4cda57b73a1e256751fee8e238992
chenjiebin <sjlinyu@qq.com>
2015-07-08 11:34:47 +0800

    php小数测试

--- php/float/float.php
+++ php/float/float.php
@@ -1,20 +1,27 @@
 <?php
 
-for ($i = 1; $i < 10; $i ++) {
-    echo $i, " ";
-    $f = ($i / 10);
-    var_dump($f * 10 == $i);
-    echo "<br />";
-}
-echo "<br />";
-
-for ($i = 1; $i < 100; $i ++) {
-    echo $i, " ";
-    $f = ($i / 100);
-    var_dump($f * 100 == $i);
-    echo "<br />";
-}
-
-
+//这里主要要完成浮点数的误差演示
 $f = 0.07;
-var_dump($f * 100 == 7);
+echo (number_format($f * 100, 20));
+//var_dump($f * 100 == 7);
+//var_dump(bccomp($f * 100, 7, 3));
+
+
+//for ($i = 1; $i < 10; $i ++) {
+//    echo $i, " ";
+//    $f = ($i / 10);
+//    var_dump($f * 10 == $i);
+//    echo "<br />";
+//}
+//echo "<br />";
+//
+//for ($i = 1; $i < 100; $i ++) {
+//    echo $i, " ";
+//    $f = ($i / 100);
+//    var_dump($f * 100 == $i);
+//    echo "<br />";
+//}
+//
+//
+//$f = 0.07;
+//var_dump($f * 100 == 7);
