1d9e6bdbcd2949fd21877d9240f48600fc25b12f
chenjiebin <sjlinyu@qq.com>
2015-07-30 11:22:10 +0800

    时间代码整理

--- php/date/index.php
+++ php/date/index.php
@@ -2,16 +2,3 @@
 
 date_default_timezone_set("Asia/Shanghai");
 
-echo getNextMonth(1448899200);
-
-function getNextMonth($time) {
-    $m = date('n', $time);
-    if ($m < 12) {
-        $nextM = $m + 1;
-        $year  = date("Y", $time);
-        return strtotime("$year-$nextM-01");
-    }
-    $nextM = 1;
-    $year  = date("Y", $time) + 1;
-    return strtotime("$year-$nextM-01");
-}
--- php/date/month.php
+++ php/date/month.php
@@ -5,10 +5,7 @@ $time = time();
 echo getMonthStart($time), "<br />";
 echo getMonthDay($time), "<br />";
 echo getMonthEnd($time), "<br />";
-var_dump(checkDateIsValid("2013-09-10")); //输出true
-var_dump(checkDateIsValid("2013-09-ha")); //输出false
-var_dump(checkDateIsValid("2012-02-29")); //输出true
-var_dump(checkDateIsValid("2013-02-29")); //输出false
+
 
 /**
  * 月初时间戳计算
@@ -45,23 +42,3 @@ function getMonthEnd($time) {
     return $monthStart + $monthSecond - 1;
 }
 
-/**
- * 校验日期格式是否正确
- *
- * @param string $date 日期
- * @param string $formats 需要检验的格式数组
- * @return boolean
- */
-function checkDateIsValid($date, $formats = array("Y-m-d", "Y/m/d")) {
-    $unixTime = strtotime($date);
-    if (!$unixTime) { //strtotime转换不对，日期格式显然不对。
-        return false;
-    }
-    //校验日期的有效性，只要满足其中一个格式就OK
-    foreach ($formats as $format) {
-        if (date($format, $unixTime) == $date) {
-            return true;
-        }
-    }
-    return false;
-}
