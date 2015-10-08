96bc7f454f8b1fa95de07553cc537e9ada78cff5
chenjiebin <sjlinyu@qq.com>
2015-10-08 22:59:03 +0800

    php国际化

--- php/gettext/hello.php
+++ php/gettext/hello.php
@@ -1,8 +1,12 @@
 <?php
 
+//设置语言文件的存放目录
 $dir    = __DIR__ . '/locale';
+//文件的名称，如该例为hello.mo
 $domain = 'hello';
+//语言类型
 $locale = 'zh_CN';
+
 
 putenv('LANG=' . $locale);
 setlocale(LC_MESSAGES, $locale);
@@ -11,4 +15,5 @@ bindtextdomain($domain, $dir);
 bind_textdomain_codeset($domain, "UTF-8");
 textdomain($domain);
 
+//_是gettext函数的简写
 echo _("Hello World!");
