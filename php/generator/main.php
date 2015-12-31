e86f2fa441f58830669c606fab3a55524c4ce8d6
chenjiebin <sjlinyu@qq.com>
2015-12-31 14:03:44 +0800

    php迭代器

---
+++ php/generator/main.php
@@ -0,0 +1,11 @@
+<?php
+
+function my_range($start, $end, $step = 1) {
+    for ($i = $start; $i <= $end; $i += $step) {
+        yield $i;
+    }
+}
+
+foreach (my_range(1, 1000) as $num) {
+    echo $num, "\n";
+}
