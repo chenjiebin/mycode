8bd08fb6a609c147c803073b5d98a97b6df3b0cc
chenjiebin <sjlinyu@qq.com>
2016-01-07 19:46:20 +0800

    spider detail

---
+++ golang/spider/our/main.go
@@ -0,0 +1,131 @@
+//使用多协程方式进行下载
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func main() {
+	//url chan
+	urlChan := make(chan string, 100)
+	//dowload response
+	respChan := make(chan *http.Response, 100)
+	//proc result
+	procRsChan := make(chan string, 100)
+
+	var down = &Downloader{urlChan: urlChan, respChan: respChan}
+	go down.Run()
+
+	var proc = &Proc{respChan: respChan, procRsChan: procRsChan}
+	go proc.Run()
+
+	var pipeline = &Pipeline{procRsChan: procRsChan}
+	go pipeline.Run()
+
+	urls := [100]string{}
+	for i := 0; i < 100; i++ {
+		urls[i] = "http://so.tv.sohu.com/list_p1122_p2122106_p3_p40_p5_p6_p73_p8_p9_p10" + strconv.Itoa(i+1) + "_p11_p12_p13.html"
+	}
+	for _, url := range urls {
+		urlChan <- url
+	}
+
+	for {
+		time.Sleep(1 * 1e9)
+	}
+
+}
+
+type Sche struct {
+}
+
+//下载器
+type Downloader struct {
+	urlChan  chan string         //url通道
+	respChan chan *http.Response //结果集通道
+}
+
+//启动页面分析器
+func (this *Downloader) Run() {
+	for {
+		url := <-this.urlChan
+		go this.Download(url)
+	}
+}
+
+//下载页面
+func (this *Downloader) Download(url string) (err error) {
+	var req *http.Request
+	req, err = http.NewRequest("GET", url, strings.NewReader(""))
+	if err != nil {
+		fmt.Println("err: ", err)
+		return
+	}
+
+	client := &http.Client{}
+	var resp *http.Response
+	resp, err = client.Do(req)
+	if err != nil {
+		fmt.Println("err: ", err)
+		return
+	}
+	fmt.Println(resp.Request.URL.String(), "download")
+	this.respChan <- resp
+	return
+}
+
+//页面分析器
+type Proc struct {
+	respChan   chan *http.Response
+	procRsChan chan string
+}
+
+//启动页面分析器
+func (this *Proc) Run() {
+	for {
+		resp := <-this.respChan
+		this.proc(resp)
+	}
+}
+
+//具体分析方法
+func (this *Proc) proc(resp *http.Response) (err error) {
+	fmt.Println(resp.Request.URL.String(), "proc")
+
+	defer resp.Body.Close()
+
+	var bodyBytes []byte
+	bodyBytes, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("err: ", err)
+		return
+	}
+
+	body := string(bodyBytes)
+	this.procRsChan <- body
+	return
+}
+
+//存储器
+type Pipeline struct {
+	procRsChan chan string
+}
+
+//启动存储器
+func (this *Pipeline) Run() {
+	for {
+		procRs := <-this.procRsChan
+		this.pipeline(procRs)
+	}
+}
+
+//处理页面分析后的结果
+func (this *Pipeline) pipeline(procRs string) (err error) {
+	fmt.Println(len(procRs))
+	return
+}
--- golang/spider/our/step4.go
+++ golang/spider/our/step4.go
@@ -1,9 +1,9 @@
 //使用多协程方式进行下载
+//增加通道将下载后的数据传递出来
 package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 	"strings"
@@ -16,21 +16,15 @@ func main() {
 		urls[i] = "http://so.tv.sohu.com/list_p1122_p2122106_p3_p40_p5_p6_p73_p8_p9_p10" + strconv.Itoa(i+1) + "_p11_p12_p13.html"
 	}
 
-	//dowload response
 	respChan := make(chan *http.Response, 100)
-	//proc result
-	procRsChan := make(chan string, 100)
 
 	var down = &Downloader{respChan: respChan}
 	for i := 0; i < len(urls); i++ {
 		go down.Download(urls[i])
 	}
 
-	var proc = &Proc{respChan: respChan, procRsChan: procRsChan}
+	var proc = &Proc{respChan: respChan}
 	go proc.Run()
-
-	var pipeline = &Pipeline{procRsChan: procRsChan}
-	go pipeline.Run()
 
 	for {
 		time.Sleep(1 * 1e9)
@@ -66,56 +60,22 @@ func (this *Downloader) Download(url string) (err error) {
 
 //页面分析器
 type Proc struct {
-	respChan   chan *http.Response
-	procRsChan chan string
+	respChan chan *http.Response //结果集通道
 }
 
 //启动页面分析器
 func (this *Proc) Run() {
 	for {
+		//等待通道传递数据，如果没有数据则会处于阻塞状态
 		resp := <-this.respChan
-		this.proc(resp)
+		this.Proc(resp)
 	}
 }
 
 //具体分析方法
-func (this *Proc) proc(resp *http.Response) (err error) {
+func (this *Proc) Proc(resp *http.Response) {
 	fmt.Println(resp.Request.URL.String(), "proc")
-
-	defer resp.Body.Close()
-
-	var bodyBytes []byte
-	bodyBytes, err = ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("err: ", err)
-		return
-	}
-
-	body := string(bodyBytes)
-	this.procRsChan <- body
-	return
 }
 
-//存储器
-type Pipeline struct {
-	procRsChan chan string
-}
-
-//启动存储器
-func (this *Pipeline) Run() {
-	for {
-		procRs := <-this.procRsChan
-		this.pipeline(procRs)
-	}
-}
-
-//处理页面分析后的结果
-func (this *Pipeline) pipeline(procRs string) (err error) {
-	fmt.Println(len(procRs))
-	return
-}
-
-//存在的问题：
-//1. 下载页面结果无法返回只能在下载器里处理
-//2. 下载协程数量没有控制，我们希望有一些类似线程池的东西，闲置状态开启若干协程，任务繁重的时候开启更多协程，但有一个最大数量
-//3. 先有url，然后才开启下载，正常来讲应该是下载器处于监听的状态，监听到有url才进行下载
+//存在的问题
+//下载完成后没有退出
