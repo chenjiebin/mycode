057ee9077172fadd42f1a2952755b738d7dee034
chenjiebin <303410541@qq.com>
2016-06-27 14:29:17 +0800

    微信授权登录功能测试——多进程,分布式统一管理access token

--- nodejs/krakentest/controllers/wechat/user.js
+++ nodejs/krakentest/controllers/wechat/user.js
@@ -5,8 +5,27 @@
  */
 'use strict';
 
+var fs = require('fs');
 var OAuth = require('wechat-oauth');
-var client = new OAuth('wx5e6a0b67b76db0ac', 'aad7585526490642a03936ff0e45157a');
+// 满足单进程的方式
+// var client = new OAuth('wx5e6a0b67b76db0ac', 'aad7585526490642a03936ff0e45157a');
+
+// 如果有多进程,多机器,则需要将access token保存到公共的服务器上
+var client = new OAuth('wx5e6a0b67b76db0ac', 'aad7585526490642a03936ff0e45157a', function (openid, callback) {
+    // 传入一个根据openid获取对应的全局token的方法
+    // 在getUser时会通过该方法来获取token
+    fs.readFile(openid + ':access_token.txt', 'utf8', function (err, txt) {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, JSON.parse(txt));
+    });
+}, function (openid, token, callback) {
+    // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+    // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+    // 持久化时请注意，每个openid都对应一个唯一的token!
+    fs.writeFile(openid + ':access_token.txt', JSON.stringify(token), callback);
+});
 
 // 测试微信授权登录
 var oauth = function (req, res, next) {
