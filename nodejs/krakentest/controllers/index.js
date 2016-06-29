2ce7aee55e29408d781f42cb4856933ebd9abd96
chenjiebin <303410541@qq.com>
2016-06-29 10:13:38 +0800

    获取当前访问host

--- nodejs/krakentest/controllers/index.js
+++ nodejs/krakentest/controllers/index.js
@@ -1,10 +1,21 @@
 'use strict';
 
-var IndexModel = require('../models/index');
+var url = require('url');
+
+// 分析当前访问的host
+var getHost = function (req, res, next) {
+    // 查看req对象
+    console.log(req);
+    console.log(req.headers.host);
+    console.log(req.originalUrl);
+    res.end(req.originalUrl);
+};
 
 module.exports = function (router) {
+    var IndexModel = require('../models/index');
     var model = new IndexModel();
     router.get('/', function (req, res) {
         res.render('index', model);
     });
+    router.get('/gethost', getHost);
 };
