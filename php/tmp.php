2ced561a96b630b5d505d0e13ca19701b904196c
chenjiebin <303410541@qq.com>
2016-06-30 16:11:07 +0800

    统计商城访问量

--- php/tmp.php
+++ php/tmp.php
@@ -1,24 +1,322 @@
-<html>
-    <head>
-        <script src="http://cdn.bootcss.com/jquery/2.2.0/jquery.min.js"></script>
-    </head>
+<?php
+echo 1;
+$str = '[
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466006400)
+            },
+            "totalView" : 15
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466092800)
+            },
+            "totalView" : 2
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466179200)
+            },
+            "totalView" : 3
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466352000)
+            },
+            "totalView" : 2
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466438400)
+            },
+            "totalView" : 46
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466524800)
+            },
+            "totalView" : 147
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466611200)
+            },
+            "totalView" : 200
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466697600)
+            },
+            "totalView" : 89
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1466784000)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae0a9b91e6ee711fa3268",
+                "day" : NumberLong(1467129600)
+            },
+            "totalView" : 6
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae165b91e6ee711fa3281",
+                "day" : NumberLong(1466006400)
+            },
+            "totalView" : 8
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae165b91e6ee711fa3281",
+                "day" : NumberLong(1466352000)
+            },
+            "totalView" : 3
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae165b91e6ee711fa3281",
+                "day" : NumberLong(1466438400)
+            },
+            "totalView" : 17
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae165b91e6ee711fa3281",
+                "day" : NumberLong(1466524800)
+            },
+            "totalView" : 69
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae165b91e6ee711fa3281",
+                "day" : NumberLong(1466611200)
+            },
+            "totalView" : 97
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae165b91e6ee711fa3281",
+                "day" : NumberLong(1466697600)
+            },
+            "totalView" : 39
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae165b91e6ee711fa3281",
+                "day" : NumberLong(1466784000)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae165b91e6ee711fa3281",
+                "day" : NumberLong(1467043200)
+            },
+            "totalView" : 5
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae190b91e6ee711fa328c",
+                "day" : NumberLong(1466006400)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae190b91e6ee711fa328c",
+                "day" : NumberLong(1466179200)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae190b91e6ee711fa328c",
+                "day" : NumberLong(1466352000)
+            },
+            "totalView" : 4
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae190b91e6ee711fa328c",
+                "day" : NumberLong(1466438400)
+            },
+            "totalView" : 19
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae190b91e6ee711fa328c",
+                "day" : NumberLong(1466524800)
+            },
+            "totalView" : 100
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae190b91e6ee711fa328c",
+                "day" : NumberLong(1466611200)
+            },
+            "totalView" : 126
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae190b91e6ee711fa328c",
+                "day" : NumberLong(1466697600)
+            },
+            "totalView" : 47
+        },
+        {
+            "_id" : {
+                "url" : "app://MallProduct/id=572ae190b91e6ee711fa328c",
+                "day" : NumberLong(1466784000)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1434988800)
+            },
+            "totalView" : 3
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1464192000)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466006400)
+            },
+            "totalView" : 43
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466092800)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466179200)
+            },
+            "totalView" : 39
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466265600)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466352000)
+            },
+            "totalView" : 39
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466438400)
+            },
+            "totalView" : 336
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466524800)
+            },
+            "totalView" : 1063
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466611200)
+            },
+            "totalView" : 1319
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466697600)
+            },
+            "totalView" : 522
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466784000)
+            },
+            "totalView" : 5
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466870400)
+            },
+            "totalView" : 5
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1466956800)
+            },
+            "totalView" : 1
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1467043200)
+            },
+            "totalView" : 14
+        },
+        {
+            "_id" : {
+                "url" : "app://TabShop/",
+                "day" : NumberLong(1467129600)
+            },
+            "totalView" : 2
+        }
+    ]';
 
-    <body>
-        <script>
-            $.ajax({
-                url: "http://192.168.1.119:10001/api/course/list/",
-                type: 'POST',
-                data: JSON.stringify({"start_time": 1456761600}),
-                cache: false,
-                dataType: "json",
-                success: function(result) {
-                    alert(result.status);
-                    alert(result.message);
-                },
-                error: function(err) {
-                    alert(err)
-                }
-            });
-        </script>
-    </body>
-</html>
+$str = str_replace(array("NumberLong(", ")"), "", $str);
+
+$arr = json_decode($str, true);
+date_default_timezone_set("Asia/Shanghai");
+$result = array();
+$names = array(
+    "app://TabShop/" => "商城首页",
+    "app://MallProduct/id=572ae0a9b91e6ee711fa3268" => "纤卡S+营养代餐粉（20餐）",
+    "app://MallProduct/id=572ae165b91e6ee711fa3281" => "纤卡S+营养代餐粉（30餐）",
+    "app://MallProduct/id=572ae190b91e6ee711fa328c" => "纤卡S+营养代餐粉（40餐）",
+);
+foreach ($arr as $v) {
+    $t = array(
+        "url" => $v["_id"]["url"],
+        "name" => $names[$v["_id"]["url"]],
+        "day" => date("Y-m-d", $v["_id"]["day"]),
+        "view" => $v["totalView"]
+    );
+    $result[] = $t;
+}
+
+
+print_r($result);
