f03517a722e9a3e0a1afef46bbd8777d5d454416
chenjiebin <303410541@qq.com>
2016-07-18 17:22:13 +0800

    crypto aes 128 cbc 加密

--- nodejs/krakentest/controllers/crypto/aes.js
+++ nodejs/krakentest/controllers/crypto/aes.js
@@ -5,45 +5,46 @@ var aes128cbc = function (req, res, next) {
 
     /**
      * 加密方法
-     * @param cryptKey 加密key
+     * @param key 加密key
      * @param iv       向量
      * @param data     需要加密的数据
      * @returns string
      */
-    var encrypt = function (cryptKey, iv, data) {
-        var encipher = crypto.createCipheriv('aes-128-cbc', cryptKey, iv);
-        var encryptData = encipher.update(data, 'utf8', 'binary');
-        encryptData += encipher.final('binary');
-        encryptData = new Buffer(encryptData, 'binary').toString('base64');
-        return encryptData;
+    var encrypt = function (key, iv, data) {
+        var cipher = crypto.createCipheriv('aes-128-cbc', key, iv);
+        var crypted = cipher.update(data, 'utf8', 'binary');
+        crypted += cipher.final('binary');
+        crypted = new Buffer(crypted, 'binary').toString('base64');
+        return crypted;
     };
 
     /**
      * 解密方法
-     * @param cryptkey 解密的key
+     * @param key      解密的key
      * @param iv       向量
-     * @param encryptData   密文
+     * @param crypted  密文
      * @returns string
      */
-    var decrypt = function (cryptkey, iv, encryptData) {
-        encryptData = new Buffer(encryptData, 'base64').toString('binary');
-        var decipher = crypto.createDecipheriv('aes-128-cbc', cryptkey, iv);
-        var decoded = decipher.update(encryptData, 'binary', 'utf8');
+    var decrypt = function (key, iv, crypted) {
+        crypted = new Buffer(crypted, 'base64').toString('binary');
+        var decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
+        var decoded = decipher.update(crypted, 'binary', 'utf8');
         decoded += decipher.final('utf8');
         return decoded;
     };
 
-    var cryptKey = '53eb59a44da52b9072601e3b3bff23eb';
-    console.log('加密的key:', cryptKey);
-    var iv = 'a2xhcgAAAAAAAAAA';
+    var key = '751f621ea5c8f930';
+    console.log('加密的key:', key.toString('hex'));
+    var iv = '2624750004598718';
+    console.log('加密的iv:', iv);
     var data = "Hello, nodejs. 演示aes-128-cbc加密和解密";
     console.log("需要加密的数据:", data);
-    var encryptData = encrypt(cryptKey, iv, data);
-    console.log("数据加密后:", encryptData);
-    var dec = decrypt(cryptKey, iv, encryptData);
+    var crypted = encrypt(key, iv, data);
+    console.log("数据加密后:", crypted);
+    var dec = decrypt(key, iv, crypted);
     console.log("数据解密后:", dec);
 
-
+    res.end('ok');
 };
 
 module.exports = function (router) {
