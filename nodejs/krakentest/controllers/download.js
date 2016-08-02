9593041c4354e891e2e9c07fedb7fe5fdcf400b5
chenjiebin <303410541@qq.com>
2016-08-02 15:38:58 +0800

    nodejs创建目录和下载图片

--- nodejs/krakentest/controllers/download.js
+++ nodejs/krakentest/controllers/download.js
@@ -2,6 +2,7 @@
 var URL = require('url');
 var request = require('request');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 // 尝试下载页面
 module.exports = function (router) {
@@ -9,10 +10,15 @@ module.exports = function (router) {
         var testUrl = 'http://cdn.firstlinkapp.com/real/i_pic/20160310/20160310/45c92b42-6d1e-4e5e-bf5e-66ae3d35cfda.png';
         var p = URL.parse(testUrl);
 
-        var savePath = appRoot + '/upload/' + p.path;
-        var stream = request(testUrl).pipe(fs.createWriteStream(savePath));
-        stream.on('finish', function () {
-            res.end(savePath);
+        var tmpArray = p.path.split('/');
+        tmpArray.pop();
+        var saveDir = appRoot + '/upload' + tmpArray.join('/');
+        mkdirp(saveDir, function (err) {
+            var savePath = appRoot + '/upload' + p.path;
+            var stream = request(testUrl).pipe(fs.createWriteStream(savePath));
+            stream.on('finish', function () {
+                res.end(savePath);
+            });
         });
     });
 };
--- nodejs/krakentest/controllers/url.js
+++ nodejs/krakentest/controllers/url.js
@@ -5,6 +5,10 @@ module.exports = function (router) {
     router.get('/', function (req, res) {
         var testUrl = 'http://cdn.firstlinkapp.com/real/i_pic/20160310/20160310/45c92b42-6d1e-4e5e-bf5e-66ae3d35cfda.png';
         var p = URL.parse(testUrl);
+
+        var tmpArray = p.path.split('/');
+        tmpArray.pop();
+        console.log(tmpArray.join('/'));
         console.log(p.href); //取到的值是：http://localhost:8888/select?aa=001&bb=002
         console.log(p.protocol); //取到的值是：http: 
         console.log(p.hostname);//取到的值是：locahost
